d7ebbe2a91c82373a31952d9662eb70ae8c34dbb
Jacob Friis Strand <jacobfriisstrand@gmail.com>
2024-09-02 17:06:54 +0200

    chore: install vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
-    domains: ["datocms-assets.com"],
+    domains: ["datocms-assets.com"]
   },
   integrations: [react()],
   devToolbar: {
-    enabled: false,
+    enabled: false
   },
+  output: "server",
+  adapter: vercel()
 });
